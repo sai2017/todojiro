dad699ab8d953e864c416607b71478177d95cfe2
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-01-12 00:11:36 +0900

    create seeds

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,6 @@ Rails.application.routes.draw do
   root to: 'home#index'
 
   namespace :api, format: 'json' do
-    resources: jiros, only: [:index, :create, :update]
+    resources :jiros, only: [:index, :create, :update]
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+3.times { Jiro.create!(name: 'Sample Jiro') }
+2.times { Jiro.create!(name: 'Sample Jiro', is_done: true) }
