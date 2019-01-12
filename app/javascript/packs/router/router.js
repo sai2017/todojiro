5b72e975d5348bda956fb50ff519afbc75f79dfd
佐々木皓晃 <ssk.hrak123@gmail.com>
2019-01-12 11:05:30 +0900

    add VueRouter

---
+++ app/javascript/packs/router/router.js
@@ -0,0 +1,17 @@
+import Vue from 'vue/dist/vue.esm.js'
+import VueRouter from 'vue-router'
+import Index from '../components/index.vue'
+import About from '../components/about.vue'
+import Contact from '../components/contact.vue'
+
+Vue.use(VueRouter)
+
+export default new VueRouter({
+  // HTMLのhistory APIを使用して、同じビュー内でURLを書き換える
+  mode: 'history',
+  routes: [
+    { path: '/', component: Index },
+    { path: '/about', component: About },
+    { path: '/contact', component: Contact },
+  ],
+})
